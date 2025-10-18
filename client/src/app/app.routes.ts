425960f8b799b56f52787ee5be2a2645b5818e37
siyuanWang348 <s.wang.73@student.scu.edu.au>
2025-10-18 21:03:06 +0800

    create search component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import {Home} from './home/home';
+import {Search} from './search/search';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home
+  },
+  {
+    path: 'search',
+    component: Search
   }
 ];
---
+++ client/src/app/search/search.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {Navbar} from '../navbar/navbar';
+
+@Component({
+  selector: 'app-search',
+  imports: [Navbar],
+  templateUrl: './search.html',
+  styleUrl: './search.css'
+})
+export class Search {
+
+}
