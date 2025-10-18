cd2a403d81a56a9fc97058f8cdc96da20ee8bc5c
siyuanWang348 <s.wang.73@student.scu.edu.au>
2025-10-18 21:06:02 +0800

    create event component and update routes

---
+++ client/src/app/event/event.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {Navbar} from '../navbar/navbar';
+
+@Component({
+  selector: 'app-event',
+  imports: [Navbar],
+  templateUrl: './event.html',
+  styleUrl: './event.css'
+})
+export class Event {
+
+}
